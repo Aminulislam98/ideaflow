ffaaead99a4fa11b62c9c156e489b65f7ae621df
Aminul islam <aminurrohman98@gmail.com>
2026-05-19 00:46:04 +0100

    social login applied

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    qualities: [70, 75, 80, 85, 90],
     remotePatterns: [
       {
         protocol: "https",
